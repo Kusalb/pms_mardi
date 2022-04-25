8298b560100c4ee109c74c51cf8fc846d929d0f9
kusalb <grand.kusal.kb@gmail.com>
2022-04-25 19:39:15 +0545

    Registration

---
+++ login.php
@@ -0,0 +1,88 @@
+<?php
+//This script will handle login
+session_start();
+
+// check if the user is already logged in
+if(isset($_SESSION['username']))
+{
+    header("location: welcome_admin.php");
+    exit;
+}
+require_once "config.php";
+
+$username = $password = "";
+$err = "";
+
+// if request method is post
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    if(empty(trim($_POST['username'])) || empty(trim($_POST['password'])))
+    {
+        $err = "Please enter username and password";
+        echo $err;
+    }
+    else{
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+    }
+
+
+    if(empty($err))
+    {
+        $sql = "SELECT id, username, password, role FROM users WHERE username = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+        $param_username = $username;
+
+        // Try to execute this statement
+        if(mysqli_stmt_execute($stmt)){
+            mysqli_stmt_store_result($stmt);
+            if(mysqli_stmt_num_rows($stmt) == 1)
+            {
+                mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $role);
+                if(mysqli_stmt_fetch($stmt))
+                {
+                    if(password_verify($password, $hashed_password))
+                    {
+                        // this means the password is corrct. Allow user to login
+                        session_start();
+                        $_SESSION["username"] = $username;
+                        $_SESSION["id"] = $id;
+                        $_SESSION["loggedin"] = true;
+                        if ($role == "Admin"){
+                        //Redirect user to welcome page
+                        header("location: welcome_admin.php");
+                        }else{
+                            header("location: welcome_user.php");
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+    }
+
+
+}
+?>
+
+
+   <h1>Php Login System</h1>
+    <a href="register.php">Register</a>
+    <a href="login.php">Login</a>
+
+
+<div class="container mt-4">
+    <h3>Please Login Here:</h3>
+    <hr>
+
+    <form action="login.php" method="post">
+            <label for="email">Username:</label>
+            <input type="text" name="username" id="email" placeholder="Enter Username">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password" placeholder="Enter Password">
+        <button type="submit" >Submit</button>
+    </form>
+
+</div>
